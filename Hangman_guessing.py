2d29b11f1d57ec445b314b953a0e79109e215fde
Jatin-gorana <105877878+Jatin-gorana@users.noreply.github.com>
2023-07-24 19:53:45 +0530

    Add files via upload

---
+++ Hangman_guessing.py
@@ -0,0 +1,4 @@
+guess_list = ['Almond', 'Banana', 'Blewits', 'Brazil', 'Carrot', 'Casaba', 'Cashew', 'Celery', 'Cherry', 'Chives',
+              'Chocho', 'Citron', 'Cob nut', 'Cooker', 'Dalkon', 'Damson', 'Endive', 'Fennel', 'Garlic', 'Girkin',
+              'Kiwano', 'Lentil', 'Lichee', 'Marrow', 'Medlar', 'Nettle', 'Orange', 'Olyster', 'Papaya', 'Pawpaw',
+              'Pepper', 'Quince', 'Radish', 'Rocket', 'Russet', 'Squash', 'Tomato', 'Turnip', 'Walnut']
