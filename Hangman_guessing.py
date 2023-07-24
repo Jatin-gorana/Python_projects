63923162ffabb283e83599ec685f8acb0c5e8b56
Jatin-gorana <105877878+Jatin-gorana@users.noreply.github.com>
2023-07-24 19:58:45 +0530

    Delete Hangman_guessing.py

--- Hangman_guessing.py
+++
@@ -1,4 +0,0 @@
-guess_list = ['Almond', 'Banana', 'Blewits', 'Brazil', 'Carrot', 'Casaba', 'Cashew', 'Celery', 'Cherry', 'Chives',
-              'Chocho', 'Citron', 'Cob nut', 'Cooker', 'Dalkon', 'Damson', 'Endive', 'Fennel', 'Garlic', 'Girkin',
-              'Kiwano', 'Lentil', 'Lichee', 'Marrow', 'Medlar', 'Nettle', 'Orange', 'Olyster', 'Papaya', 'Pawpaw',
-              'Pepper', 'Quince', 'Radish', 'Rocket', 'Russet', 'Squash', 'Tomato', 'Turnip', 'Walnut']
